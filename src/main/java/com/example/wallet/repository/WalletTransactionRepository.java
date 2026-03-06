44a012462983b2af108428a63297e238eb2e7ff5
AryNov <arya.nov.pratama@gmail.com>
2026-03-06 20:30:14 +0700

    Feat/wallettransaction (#7)
    * created WalletTransaction and WalletTransactionRepository files
    
    * setup WalletTransactionRepository
    
    * created findbywalletid()function

--- src/main/java/com/example/wallet/repository/WalletTransactionRepository.java
+++ src/main/java/com/example/wallet/repository/WalletTransactionRepository.java
@@ -4,5 +4,7 @@ import com.example.wallet.model.WalletTransaction;
 import org.springframework.data.jpa.repository.JpaRepository;
 import java.util.List;
 
-public interface WalletTransactionRepository extends JpaRepository<WalletTransaction, String>{
+public interface WalletTransactionRepository extends JpaRepository<WalletTransaction, String> {
+
+    List<WalletTransaction> findByWalletId(String walletId);
 }
