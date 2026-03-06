3c82f5d6838cb16bd663fd8515bd2f079d64e385
AryNov <arya.nov.pratama@gmail.com>
2026-03-06 20:14:38 +0700

    setup WalletTransactionRepository

--- src/main/java/com/example/wallet/repository/WalletTransactionRepository.java
+++ src/main/java/com/example/wallet/repository/WalletTransactionRepository.java
@@ -1,4 +1,8 @@
 package com.example.wallet.repository;
 
-public class WalletTransactionRepository {
+import com.example.wallet.model.WalletTransaction;
+import org.springframework.data.jpa.repository.JpaRepository;
+import java.util.List;
+
+public interface WalletTransactionRepository extends JpaRepository<WalletTransaction, String> {
 }
