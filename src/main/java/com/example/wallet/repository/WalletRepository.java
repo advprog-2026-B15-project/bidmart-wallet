86d5cce82bee8e030672fe6a525bfe3466a18c05
AryNov <arya.nov.pratama@gmail.com>
2026-03-06 18:04:07 +0700

    created files for wallet repository and services

---
+++ src/main/java/com/example/wallet/repository/WalletRepository.java
@@ -0,0 +1,8 @@
+package com.example.wallet.repository;
+
+import com.example.wallet.model.Wallet;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+
+public class WalletRepository {
+}
