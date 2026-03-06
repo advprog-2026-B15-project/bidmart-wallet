ba924a0130f197dc1c6d3478361429976769b90a
AryNov <arya.nov.pratama@gmail.com>
2026-03-06 20:16:17 +0700

    added getwallet() function to service

--- src/main/java/com/example/wallet/service/WalletService.java
+++ src/main/java/com/example/wallet/service/WalletService.java
@@ -1,4 +1,22 @@
 package com.example.wallet.service;
 
+import com.example.wallet.model.Wallet;
+import com.example.wallet.model.WalletTransaction;
+import com.example.wallet.repository.WalletRepository;
+import com.example.wallet.repository.WalletTransactionRepository;
+import org.springframework.stereotype.Service;
+
+import java.math.BigDecimal;
+import java.util.List;
+
+@Service
 public class WalletService {
+
+    private final WalletRepository walletRepository;
+    private final WalletTransactionRepository transactionRepository;
+
+    public WalletService(WalletRepository walletRepository,WalletTransactionRepository transactionRepository) {
+        this.walletRepository = walletRepository;
+        this.transactionRepository = transactionRepository;
+    }
 }
