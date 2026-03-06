099e6b6485b19838246c59a090adba86afb22197
AryNov <arya.nov.pratama@gmail.com>
2026-03-06 20:23:27 +0700

    Feat/wallet getwallet createwallet (#6)
    * added findbyuserId function
    
    * fixed syntax issue
    
    * added getwallet() function to service
    
    * created getwallet() and createwallet() function to walletservice

--- src/main/java/com/example/wallet/service/WalletService.java
+++ src/main/java/com/example/wallet/service/WalletService.java
@@ -1,4 +1,33 @@
 package com.example.wallet.service;
 
+import com.example.wallet.model.Wallet;
+import com.example.wallet.model.WalletTransaction;
+import com.example.wallet.repository.WalletRepository;
+import com.example.wallet.repository.WalletTransactionRepository;
+import org.springframework.stereotype.Service;
+
+import java.math.BigDecimal;
+import java.util.List;
+
+@Service
 public class WalletService {
+
+    private final WalletRepository walletRepository;
+    private final WalletTransactionRepository transactionRepository;
+
+    public WalletService(WalletRepository walletRepository,WalletTransactionRepository transactionRepository) {
+        this.walletRepository = walletRepository;
+        this.transactionRepository = transactionRepository;
+    }
+
+    public Wallet getWallet(String userId) {
+        return walletRepository.findByUserId(userId)
+                .orElseGet(() -> createWallet(userId));
+    }
+
+    public Wallet createWallet(String userId) {
+        Wallet wallet = new Wallet();
+        wallet.setUserId(userId);
+        return walletRepository.save(wallet);
+    }
 }
