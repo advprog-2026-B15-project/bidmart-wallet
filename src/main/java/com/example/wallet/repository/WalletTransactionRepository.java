ad05ad1bb8fa661375e0949d1d2254b5ddcea46d
AryNov <arya.nov.pratama@gmail.com>
2026-03-06 20:28:26 +0700

    created findbywalletid()function

--- src/main/java/com/example/wallet/repository/WalletTransactionRepository.java
+++ src/main/java/com/example/wallet/repository/WalletTransactionRepository.java
@@ -5,4 +5,6 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import java.util.List;
 
 public interface WalletTransactionRepository extends JpaRepository<WalletTransaction, String> {
+
+    List<WalletTransaction> findByWalletId(String walletId);
 }
