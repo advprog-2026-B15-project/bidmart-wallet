1fbac8c709a510dd7ce7c670521bfae277bcd744
AryNov <arya.nov.pratama@gmail.com>
2026-04-16 16:52:20 +0700

    created DTO for AmountRequest

---
+++ src/main/java/com/example/wallet/DTO/AmountRequest.java
@@ -0,0 +1,18 @@
+package com.example.wallet.DTO;
+
+import java.math.BigDecimal;
+
+public class AmountRequest {
+
+    private BigDecimal amount;
+
+    public AmountRequest() {}
+
+    public BigDecimal getAmount() {
+        return amount;
+    }
+
+    public void setAmount(BigDecimal amount) {
+        this.amount = amount;
+    }
+}
