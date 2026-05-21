e0a92586b1c05fde47e444426f345589ba71412c
AryNov <arya.nov.pratama@gmail.com>
2026-05-21 15:40:11 +0700

    created exception handler for bad http request, mostly for bug finding

---
+++ src/main/java/com/exception/GlobalExceptionHandler.java
@@ -0,0 +1,23 @@
+package com.exception;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+import java.util.Map;
+
+//NOTE : i made this for bug finding no need to use in final product
+@RestControllerAdvice
+public class GlobalExceptionHandler {
+
+    @ExceptionHandler(RuntimeException.class)
+    public ResponseEntity<Map<String, String>> handleRuntimeException(RuntimeException ex) {
+        return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(
+                Map.of(
+                        "code", "BAD_REQUEST",
+                        "message", ex.getMessage()
+                )
+        );
+    }
+}
